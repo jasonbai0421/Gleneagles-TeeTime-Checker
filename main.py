27b201c866c56f432c7cb71ee433ba7c34e6430c
jasonbai0421 <jason_bai@126.com>
2025-06-09 07:59:18 -0700

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def send_email(subject, body):
     msg["To"] = EMAIL_RECEIVER
 
     try:
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        with smtplib.SMTP_SSL("smtp.126.com", 25) as server:
             server.login(EMAIL_SENDER, EMAIL_PASSWORD)
             server.sendmail(EMAIL_SENDER, EMAIL_RECEIVER, msg.as_string())
         print("✅ 邮件发送成功")
