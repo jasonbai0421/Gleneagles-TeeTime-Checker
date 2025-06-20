11a4864557db5cf32d5002109f0ab194c8bec07b
jasonbai0421 <jason_bai@126.com>
2025-06-19 19:51:21 -0700

    Update northlands.py

--- northlands.py
+++ northlands.py
@@ -7,6 +7,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from webdriver_manager.chrome import ChromeDriverManager
 from email.mime.text import MIMEText
 import smtplib
