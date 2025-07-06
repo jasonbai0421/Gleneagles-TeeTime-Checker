3059e58c47e2af018d325e7ab0b9556e8b409d77
jasonbai0421 <jason_bai@126.com>
2025-07-06 11:11:08 -0700

    Update gleneagles.py

--- gleneagles.py
+++ gleneagles.py
@@ -25,13 +25,13 @@ from google.oauth2.service_account import Credentials
 
 ## 获取 Google Sheet 中用户配置
 def load_user_preferences():
+    scopes = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+    
     # 从环境变量中读取 JSON 字符串（必须是 GitHub Secret 中设置的内容）
     credentials_file = "teetime-465103-5096aca64eb6.json"
     if not os.path.exists(credentials_file):
         raise ValueError("❌ Credentials JSON file not found.")
-    creds = Credentials.from_service_account_file(credentials_file, scopes=scopes)
-    
-    scopes = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+    creds = Credentials.from_service_account_file(credentials_file, scopes=scopes)    
 
     # 解析 JSON 字符串为字典
     info = json.loads(credentials_json_str)
